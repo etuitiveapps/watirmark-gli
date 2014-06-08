0154ec04a19e009f8880e3b8d818969840e06e9e
blackbaud-napoleonkernessant <napoleon.kernessant@blackbaud.com>
2014-06-07 21:34:44 -0500

    create new watirmark project in the current directory

--- lib/watirmark-gli/new.rb
+++ lib/watirmark-gli/new.rb
@@ -1,8 +1,19 @@
+require 'fileutils'
 module WatirmarkGli
   # Create new WatirmarkGli project
   class Project
+    def self.initialize(file_name)
+      file_relative_path = File.dirname(__FILE__)
+      @relative_path = "#{file_relative_path}/#{file_name}"
+    end
+
     def self.new(file_name)
-      puts "Project #{file_name} created"
+      initialize(file_name)
+      create_directory(@relative_path)
+    end
+
+    def self.create_directory(directory_path)
+      FileUtils.mkdir_p directory_path
     end
   end
 end
